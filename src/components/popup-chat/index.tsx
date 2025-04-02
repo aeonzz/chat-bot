05486d1a20a97f39495fe5f5524a18077367a2a6
aeonzz <christiancaneos1@gmail.com>
2025-04-02 15:27:45 +0800

    feat: implement PopupChat component and update main entry point for chatbot integration

--- src/components/popup-chat/index.tsx
+++ src/components/popup-chat/index.tsx
@@ -41,126 +41,135 @@ export default function PopupChat() {
   }, [open, setContent]);
 
   return (
-    <div className="flex min-h-screen justify-center">
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            size="icon"
-            className="absolute right-4 bottom-4 size-10 rounded-full"
-          >
-            <Bot />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent
-          collisionPadding={20}
-          className="flex h-[553px] w-[449px] flex-col p-0"
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          size="icon"
+          className="absolute right-4 bottom-4 size-10 rounded-full"
         >
-          <div className="bg-primary relative flex h-[48px] w-full items-center justify-between rounded-t-xl px-4 py-[12px]">
-            <img
-              src="/papaz-handsup.svg"
-              alt="Papa Z Image"
-              className="absolute -bottom-2 -left-9"
-            />
-            <h4 className="text-primary-foreground text-4 ml-20 leading-[24px] font-semibold tracking-normal">
-              Papa Z’s here to help
-            </h4>
-            <Button
-              variant="default"
-              size="icon"
-              className="size-6 border-2 bg-transparent"
-              onClick={() => setOpen((prev) => !prev)}
-            >
-              <X className="text-primary-foreground" />
-            </Button>
-          </div>
-          {messages.length > 0 ? (
-            <div className="mb-2 h-full w-full overflow-y-auto px-4 py-3">
-              <div className="mx-auto mt-3 max-w-2xl">
-                {messages.map((msg, i) => (
+          <Bot />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        collisionPadding={20}
+        className="flex h-[553px] w-[449px] flex-col p-0"
+      >
+        <div className="bg-primary relative flex h-[48px] w-full items-center justify-between rounded-t-xl px-4 py-[12px]">
+          <img
+            src={
+              import.meta.env.PROD
+                ? "./dist/papaz-handsup.svg"
+                : "/papaz-handsup.svg"
+            }
+            alt="Papa Z Image"
+            className="absolute -bottom-2 -left-9"
+          />
+          <h4 className="text-primary-foreground text-4 ml-20 leading-[24px] font-semibold tracking-normal">
+            Papa Z’s here to help
+          </h4>
+          <Button
+            variant="default"
+            size="icon"
+            className="size-6 border-2 bg-transparent"
+            onClick={() => setOpen((prev) => !prev)}
+          >
+            <X className="text-primary-foreground" />
+          </Button>
+        </div>
+        {messages.length > 0 ? (
+          <div className="mb-2 h-full w-full overflow-y-auto px-4 py-3">
+            <div className="mx-auto mt-3 max-w-2xl">
+              {messages.map((msg, i) => (
+                <div
+                  key={i}
+                  className={cn(
+                    "mb-5 flex",
+                    msg.role === "user" ? "justify-end" : "justify-start"
+                  )}
+                >
                   <div
-                    key={i}
                     className={cn(
-                      "mb-5 flex",
-                      msg.role === "user" ? "justify-end" : "justify-start"
+                      "flex w-[320px] flex-col gap-3",
+                      msg.role === "assistant" ? "items-start" : "items-end"
                     )}
                   >
-                    <div
+                    <p
                       className={cn(
-                        "flex w-[320px] flex-col gap-3",
-                        msg.role === "assistant" ? "items-start" : "items-end"
+                        "w-fit rounded-lg px-3 py-2 text-[12px] font-medium tracking-normal",
+                        msg.role === "assistant"
+                          ? "bg-secondary text-foreground"
+                          : "bg-accent text-accent-foreground",
+                        msg.error &&
+                          "border-destructive/30 bg-destructive/20 border"
                       )}
                     >
-                      <p
-                        className={cn(
-                          "w-fit rounded-lg px-3 py-2 text-[12px] font-medium tracking-normal",
-                          msg.role === "assistant"
-                            ? "bg-secondary text-foreground"
-                            : "bg-accent text-accent-foreground",
-                          msg.error &&
-                            "border-destructive/30 bg-destructive/20 border"
-                        )}
-                      >
-                        {msg.content}
-                      </p>
-                      {/* {msg.sources.length > 0 && (
+                      {msg.content}
+                    </p>
+                    {/* {msg.sources.length > 0 && (
                         <div className="grid w-full grid-cols-2 gap-2">
                           {msg.sources.map((source, i) => (
                             <SourceCard key={i} source={source} />
                           ))}
                         </div>
                       )} */}
-                    </div>
                   </div>
-                ))}
-                {isLoading && (
-                  <div className="[&_div]:bg-primary flex items-center [&_div]:mx-0.5 [&_div]:size-1.5 [&_div]:rounded-full">
-                    <div className="animate-bounce delay-0" />
-                    <div className="animate-bounce delay-150" />
-                    <div className="animate-bounce delay-300" />
-                  </div>
-                )}
-                <div ref={messagesEndRef} />
-              </div>
+                </div>
+              ))}
+              {isLoading && (
+                <div className="[&_div]:bg-primary flex items-center [&_div]:mx-0.5 [&_div]:size-1.5 [&_div]:rounded-full">
+                  <div className="animate-bounce delay-0" />
+                  <div className="animate-bounce delay-150" />
+                  <div className="animate-bounce delay-300" />
+                </div>
+              )}
+              <div ref={messagesEndRef} />
             </div>
-          ) : (
-            <div className="flex h-full flex-col items-center justify-center gap-2.5 text-center">
-              <img src="/papaz-holdingmic.svg" alt="Papa Z Image" />
-              <div>
-                <h5 className="text-secondary-foreground text-[16px] leading-5 font-medium">
-                  Do you have some questions?
-                </h5>
-                <p className="text-sm leading-5">I can help you with that.</p>
-              </div>
-            </div>
-          )}
-          <div className="flex-1 px-3">
-            <ScrollArea className="flex w-full items-center gap-x-2.5">
-              <div className="w-max space-x-2.5 px-1 py-1 pb-4">
-                {topics.map((topic, index) => (
-                  <Badge
-                    key={index}
-                    variant="secondary"
-                    className="hover:ring-ring cursor-pointer leading-5 font-normal hover:ring-1"
-                    onClick={() => setContent(topic)}
-                  >
-                    {topic}
-                  </Badge>
-                ))}
-              </div>
-              <ScrollBar orientation="horizontal" />
-            </ScrollArea>
           </div>
-          <div className="p-4 pt-0">
-            <ChatBox
-              content={content}
-              isLoading={isLoading}
-              onSubmit={onSubmit}
-              setContent={setContent}
-              ref={chatBoxRef}
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center gap-2.5 text-center">
+            <img
+              src={
+                import.meta.env.PROD
+                  ? "./dist/papaz-holdingmic.svg"
+                  : "/papaz-holdingmic.svg"
+              }
+              alt="Papa Z Image"
             />
+            <div>
+              <h5 className="text-secondary-foreground text-[16px] leading-5 font-medium">
+                Do you have some questions?
+              </h5>
+              <p className="text-sm leading-5">I can help you with that.</p>
+            </div>
           </div>
-        </PopoverContent>
-      </Popover>
-    </div>
+        )}
+        <div className="flex-1 px-3">
+          <ScrollArea className="flex w-full items-center gap-x-2.5">
+            <div className="w-max space-x-2.5 px-1 py-1 pb-4">
+              {topics.map((topic, index) => (
+                <Badge
+                  key={index}
+                  variant="secondary"
+                  className="hover:ring-ring cursor-pointer leading-5 font-normal hover:ring-1"
+                  onClick={() => setContent(topic)}
+                >
+                  {topic}
+                </Badge>
+              ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        </div>
+        <div className="p-4 pt-0">
+          <ChatBox
+            content={content}
+            isLoading={isLoading}
+            onSubmit={onSubmit}
+            setContent={setContent}
+            ref={chatBoxRef}
+          />
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.tsx";
+// import App from "./App.tsx";
+import PopupChat from "./components/popup-chat";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+window.renderChatbot = function () {
+  const container = document.createElement('div')
+  container.id = 'chatbot-widget'
+  document.body.appendChild(container)
+
+  const root = createRoot(container)
+  root.render(<PopupChat />)
+};
+
+declare global {
+  interface Window {
+    renderChatbot: () => void;
+  }
+}
